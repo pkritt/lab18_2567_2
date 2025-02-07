e57807c23f1d93a359943042d76f2b5e26f4c2bb
pkritt <krittapat37819@gmail.com>
2025-02-07 20:14:26 +0700

    #

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+
+struct Rect{
+	double x,y,w,h;
+};
+
+double overlap(Rect* X,Rect* Y){
+     double a = max(0.0, min(X->x + X->w, Y->x + Y->w) - max(X->x, Y->x));
+     double b = max(0.0, min(X->y, Y->y) - max(X->y - X->h, Y->y - Y->h));
+    return a * b;
+}
 
